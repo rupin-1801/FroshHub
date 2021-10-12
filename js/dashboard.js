7bb76790698a0f9ad3f3ae0e12f4cd93cae96a00
rupin-1801 <rupinbhugra1411@gmail.com>
2021-10-12 22:41:41 +0530

    updated dashboard design

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,16 +1,25 @@
 const containerTop = document.getElementById("fh-container-top");
 const click = document.getElementById("click");
+const rightTab = document.getElementById("right-box");
+
+const postTop = rightTab.offsetTop-20;
+
+document.onscroll = () => {
+    if(window.pageYOffset >= postTop){
+        rightTab.classList.add("fh-sticky");
+    }
+    else if(window.pageYOffset < postTop){
+        rightTab.classList.remove("fh-sticky");
+    }
+}
 
 const cardMove = () => {
     let child = containerTop.children[0];
     value = child.clientWidth + ((containerTop.offsetWidth - (child.offsetWidth * 3))/3);
-    console.log(value);
     if(containerTop.scrollLeft > (containerTop.children.length - 4) * value)
     containerTop.scrollLeft = 0;
     else
     containerTop.scrollLeft += value;
-    console.log(containerTop.scrollLeft, 
-        (containerTop.children.length - 4) * value);
 }
 
 window.onload = () => {
