06b86f35147f4a9773edcad87100d5d61fd63362
rupin-1801 <rupinbhugra1411@gmail.com>
2021-10-16 15:07:05 +0530

    added new post button at 425 and stories card responsiveness, removed edit button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var store_email, email, pass, remembered;
+var emailCode, email, pass, remembered;
 
 const remember = document.getElementById("remember");
 const mRemember = document.getElementById("m-remember");
@@ -6,36 +6,36 @@ const eye = document.getElementsByClassName("show")[0];
 const passField = document.getElementById("pass");
 
 eye.onclick = () => {
-  if(eye.classList.contains("far")){
+  if (eye.classList.contains("far")) {
     eye.classList.remove("fa-eye");
     eye.classList.remove("far");
     eye.classList.add("fa-eye-slash");
     eye.classList.add("fas");
-    passField.type="text";
-    passField.style.letterSpacing ="0px";
-  }
-  else{
+    passField.type = "text";
+    passField.style.letterSpacing = "0px";
+  } else {
     eye.classList.remove("fa-eye-slash");
     eye.classList.remove("fas");
     eye.classList.add("fa-eye");
-    eye.classList.add("far");    
-    passField.type="password";
-    passField.style.letterSpacing ="3px";
+    eye.classList.add("far");
+    passField.type = "password";
+    passField.style.letterSpacing = "3px";
   }
-}
+};
 
 window.onload = () => {
-  // firebase.database().ref("student/" + "test")
+  // firebase.database().ref("student/" + "testglacom")
   // .set({
   //   email: "test@gla.com",
   //   password: "froshHubTeam"
   // });
-  if(sessionStorage.getItem("FRID")){
+  if (sessionStorage.getItem("FRID")) {
     window.location = "./pages/homepage.html";
   }
   if (localStorage.getItem("remembered")) {
     document.getElementById("mail").value = localStorage.getItem("user-email");
-    document.getElementById("m-mail").value = localStorage.getItem("user-email");
+    document.getElementById("m-mail").value =
+      localStorage.getItem("user-email");
     document.getElementById("pass").value = localStorage.getItem("user-pass");
     document.getElementById("m-pass").value = localStorage.getItem("user-pass");
   }
@@ -44,6 +44,11 @@ window.onload = () => {
 function readForm(form) {
   email = document.getElementById(form[0].id).value;
   pass = document.getElementById(form[1].id).value;
+  let e1 = email.split("@")[0];
+  let e2 = email.split("@")[1];
+  e1=e1.split(".").join("");
+  e2=e2.split(".").join("");
+  emailCode = e1+e2;
 }
 
 remember.addEventListener("change", () => {
@@ -58,28 +63,34 @@ function verification(event) {
   readForm(event.target);
   let loading = event.target.childNodes[event.target.childNodes.length - 2];
   event.preventDefault();
-  loading.style.display="block";
+  loading.style.display = "block";
   let i = 0;
   const loadEvent = setInterval(() => {
-    if(i%4 === 0) loading.innerHTML = "Validating<b>.</b>...";
-    if(i%4 === 1) loading.innerHTML = "Validating.<b>.</b>..";
-    if(i%4 === 2) loading.innerHTML = "Validating..<b>.</b>.";
-    if(i%4 === 3) loading.innerHTML = "Validating...<b>.</b>";
+    if (i % 4 === 0) loading.innerHTML = "Validating<b>.</b>...";
+    if (i % 4 === 1) loading.innerHTML = "Validating.<b>.</b>..";
+    if (i % 4 === 2) loading.innerHTML = "Validating..<b>.</b>.";
+    if (i % 4 === 3) loading.innerHTML = "Validating...<b>.</b>";
     i++;
   }, 200);
-  firebase.database().ref("student/" + email.split("@")[0])
-  .on("value", function(snap){
-    clearInterval(loadEvent);
-    loading.style.display="none";
-    if((email !== snap.val().email) || (pass !== snap.val().password)){
-      alert("Invalid email id or password");
-    }
-    else{
-      sessionStorage.setItem("FRID", email);
-      sessionStorage.setItem("FRSE", pass);
-      window.location = "./pages/homepage.html";
-    }
-  })
+  firebase
+    .database()
+    .ref("student/" + emailCode)
+    .on("value", function (snap) {
+      clearInterval(loadEvent);
+      loading.style.display = "none";
+      if(snap.val() != null){
+        if (email !== snap.val().email || pass !== snap.val().password) {
+          alert("Invalid email id or password");
+        } else {
+          sessionStorage.setItem("FRID", email);
+          sessionStorage.setItem("FRSE", pass);
+          window.location = "./pages/homepage.html";
+        }
+      }
+      else{
+        alert("User Does not exists.");
+      }
+    });
 
   if (remembered) {
     localStorage.setItem("remembered", true);
