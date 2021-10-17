8e3a3a4a16b8509667eec22c3a5752094072682d
rupin-1801 <rupinbhugra1411@gmail.com>
2021-10-17 19:31:45 +0530

    new post add functionality completed

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,25 +8,27 @@ const addPost = document.getElementsByClassName("fh-add-post")[0];
 const backPost = document.getElementById("post-back");
 const publish = document.getElementById("publish");
 const postTop = rightTab.offsetTop - 20;
-
+const formatList = document.getElementsByClassName("format-item");
 const newTag = document.getElementById("tag");
 const newRole = document.getElementById("role");
 const newMessage = document.getElementById("message");
 
-var openAddPost = false;
+var openAddPost = false,
+  orlistOn = false,
+  unlistOn = false;
 
 const postData = {
-    name: "Rupin Bhugra",
-    tag: "Getting Started",
-    role: "Admin",
-    message: `This is my first post to make you familiar with how these posts works.
-    so there are 3 buttons given below: <br /><br /><b>1. Like: </b>To tell whether this content is
-    helpful for you or not.<br /><b>2. Comment: </b>To add your thoughts about the post.<br /><b>3. Share:
-    </b> Share to your groups.`,
-    comment: "4",
-    like: "12",
-    time: "2h ago"
-}
+  name: "Rupin Bhugra",
+  tag: "Getting Started",
+  role: "Admin",
+  message: `This is my first post to make you familiar with how these posts works.
+    so there are 3 buttons given below: <ol><li><b> Like: </b>To tell whether this content is
+    helpful for you or not.<li><b>Comment: </b>To add your thoughts about the post.<li><b>Share:
+    </b> Share to your groups.</ul>`,
+  comment: "4",
+  like: "12",
+  time: "2h ago",
+};
 rightTab.onclick = (event) => {
   event.stopPropagation();
 };
@@ -76,7 +78,7 @@ const cardMove = () => {
 
 window.onload = () => {
   cardInterval = setInterval(cardMove, 2000);
-  for(let i = 0; i < 3; i++) createStory(postData);
+  for (let i = 0; i < 3; i++) createStory(postData);
 };
 containerTop.addEventListener("mouseleave", () => {
   cardInterval = setInterval(cardMove, 2000);
@@ -102,7 +104,7 @@ function createStory(data) {
                 </p>
             </div>
         </header>
-        <p class="card-text">${data.message}</p>
+        <div class="card-text">${data.message}</div>
     </div>
     <div class="card-footer">
         <ul class="nav nav-pills nav-fill">
@@ -122,26 +124,192 @@ function createStory(data) {
   stories.prepend(fhStory);
 }
 
-function readPostForm(){
-    let data = {
-        name:"New Student",
-        comment: 0,
-        like: 0,
-        time: "Now",
-    };
-    data.tag = newTag.value;
-    data.role = newRole.value;
-    data.message = newMessage.value;
-    return data;
+function readPostForm() {
+  let data = {
+    name: "New Student",
+    comment: 0,
+    like: 0,
+    time: "now",
+  };
+  data.tag = newTag.value;
+  data.role = newRole.value;
+  data.message = newMessage.value;
+  return data;
 }
+function useFormat(type) {
+  console.log("aya");
+  let start = newMessage.selectionStart;
+  let end = newMessage.selectionEnd;
+  let text = newMessage.value;
+  if (type === "B") {
+    newMessage.value =
+      text.slice(0, start) +
+      "<b>" +
+      text.slice(start, end) +
+      "</b>" +
+      text.slice(end, text.length);
+    newMessage.focus();
+    if (end - start > 0) newMessage.selectionEnd = end + 7;
+    else newMessage.selectionEnd = start + 3;
+  } else if (type === "I") {
+    newMessage.value =
+      text.slice(0, start) +
+      "<i>" +
+      text.slice(start, end) +
+      "</i>" +
+      text.slice(end, text.length);
+    newMessage.focus();
+    if (end - start > 0) newMessage.selectionEnd = end + 7;
+    else newMessage.selectionEnd = start + 3;
+  } else if (type === "U") {
+    newMessage.value =
+      text.slice(0, start) +
+      "<u>" +
+      text.slice(start, end) +
+      "</u>" +
+      text.slice(end, text.length);
+    newMessage.focus();
+    if (end - start > 0) newMessage.selectionEnd = end + 7;
+    else newMessage.selectionEnd = start + 3;
+  } 
+  else if (type === "OL") {
+    newMessage.value =
+      text.slice(0, start) +
+      "<ol><li>" +
+      text.slice(start, end) +
+      "</ol>" +
+      text.slice(end, text.length);
+    newMessage.focus();
+    if (end - start > 0) newMessage.selectionEnd = end + 13;
+    else newMessage.selectionEnd = start + 8;
+  } 
+  else if (type === "UL") {
+    newMessage.value =
+      text.slice(0, start) +
+      "<ul><li>" +
+      text.slice(start, end) +
+      "</ul>" +
+      text.slice(end, text.length);
+    newMessage.focus();
+    if (end - start > 0) newMessage.selectionEnd = end + 13;
+    else newMessage.selectionEnd = start + 8;
+  } else if (type === "L") {
+    newMessage.value =
+      text.slice(0, start) +
+      `<a href='' target='_blank'>` +
+      text.slice(start, end) +
+      "</a>" +
+      text.slice(end, text.length);
+    newMessage.focus();
+    if (end - start > 0) newMessage.selectionEnd = start + 9;
+    else newMessage.selectionEnd = start + 11;
+  } else if (type === "LI") {
+    newMessage.value =
+      text.slice(0, start) +
+      "<li>" +
+      text.slice(start, end) +
+      text.slice(end, text.length);
+    newMessage.focus();
+    newMessage.selectionEnd = start + 4;
+  }
+}
+
+for (let i = 0; i < 6; i++) {
+  formatList[i].addEventListener("click", () => {
+    console.log(i);
+    if (i == 0) useFormat("B");
+    if (i == 1) useFormat("I");
+    if (i == 2) useFormat("U");
+    if (i == 3) useFormat("OL");
+    if (i == 4) useFormat("UL");
+    if (i == 5) useFormat("L");
+  });
+}
+newMessage.addEventListener("keydown", (event) => {
+  message = newMessage.value;
+  
+  if (
+    event.key === "Backspace" &&
+    message[newMessage.selectionEnd - 1] === ">"
+    ) {
+      event.preventDefault();
+      closing = false;
+      for (let i = newMessage.selectionEnd - 1; i >= 0; i--) {
+        if (message[i] === "<") {
+          openFirst = i;
+          if(closing) curTag = message.slice(i+2, newMessage.selectionEnd-1);
+          else curTag = message.slice(i+1, newMessage.selectionEnd-1);
+          break;
+        } else if (message[i] === "/") {
+          closing = true;
+        }
+      }
+      
+      if (closing) {
+        close = false;
+      for (let i = openFirst; i >= 0; i--) {
+        if (message[i] === ">" && !close) {
+          closeSecond = i;
+          close = true;
+        }
+        if (close && message[i] === "<") {
+          if(message.slice(i+1, closeSecond) === curTag){
+            openSecond = i;
+            break;
+          }
+          else{
+            close = false;
+          }
+        }
+      }
+        newMessage.value =
+        message.slice(0, openSecond ) +
+        message.slice(closeSecond + 1, openFirst) +
+        message.slice(newMessage.selectionEnd, message.length);
+        newMessage.selectionEnd = openSecond + (openFirst - closeSecond - 1);
+    } 
+    else {
+      open = false;
+      for (let i = newMessage.selectionEnd; i < message.length; i++) {
+        if (message[i] === "<" && !open) {
+          openSecond = i;
+          open = true;
+        }
+        if (open && message[i] === ">") {
+          closeSecond = i;
+          break;
+        }
+        }
+      start = newMessage.selectionEnd;
+      newMessage.value =
+      message.slice(0, openFirst) +
+      message.slice(newMessage.selectionEnd, openSecond) +
+      message.slice(closeSecond + 1, message.length);
+      newMessage.selectionEnd = openFirst + (openSecond - start);
+    }
+  }
+
+  li = message.slice(newMessage.selectionEnd, newMessage.selectionEnd + 4);
+  tag = message.slice(newMessage.selectionEnd, newMessage.selectionEnd + 5);
+  if (
+    event.key === "Enter" &&
+    (li === "<li>" || tag === "</ol>" || tag === "</ul>")
+  ) {
+    event.preventDefault();
+    useFormat("LI");
+  }
+});
 
 publish.addEventListener("click", (event) => {
   event.preventDefault();
   let data = readPostForm();
-  if(data.tag !== "" && data.role !== "" && data.message !== ""){
+  if (
+    //   data.tag !== "" && data.role !== "" &&
+    data.message !== ""
+  ) {
     createStory(data);
-    newTag.value="";
-    newRole.value="";
-    newMessage.value="";
+    newTag.value = "";
+    newRole.value = "";
+    newMessage.value = "";
   }
 });
