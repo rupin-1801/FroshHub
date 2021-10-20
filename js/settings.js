91da627197b38644b60ef46c79f6b826e2664703
Harsh Gautam <harsh.gautam_cs19@gla.ac.in>
2021-10-20 18:09:15 +0530

    Added Update and remove profile picture functionality

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,15 @@
 function goBack() {
   window.history.back();
 }
+
+var loadFile = function (event) {
+  var image = document.getElementById("output");
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+function removeProfile(event) {
+  var image = document.getElementById("output");
+  // output.parentNode.removeChild(output);
+  image.src = "https://bootdey.com/img/Content/avatar/avatar1.png";
+  // URL.revokeObjectURL(image.src);
+}
