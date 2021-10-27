a3e0effcd800a685ac66be661737c4c55e0ac2d3
mayank190821 <mayank.123.2000@gmail.com>
2021-10-27 11:43:35 +0530

    connect with firebase

--- js/group.js
+++ js/group.js
@@ -1,6 +1,20 @@
 var input_bar = document.getElementById("input-bar");
 var arrow = document.getElementById("location-arrow");
 var emoji = document.querySelector("#emoji");
+var selectedEmoji = document.getElementsByClassName("selectEmoji");
+const chatTime = document.getElementsByClassName("time");
+var curUserName = "Group 1";
+window.onload = () => {
+    var userName = document.querySelector("#user-name>h2");
+    let fName = document.getElementById("name");
+    userName.innerHTML = fName.innerText;
+}
+for (let i = 0; i < selectedEmoji.length; i++) {
+    selectedEmoji[i].addEventListener('click', (e) => {
+        input_bar.value += e.target.innerHTML;
+    })
+}
+var curUserName="";
 emoji.addEventListener('click', () => {
     var emojiBox = document.querySelector('.emojiPicker')
     if (!emoji.checked) {
@@ -10,16 +24,12 @@ emoji.addEventListener('click', () => {
         emojiBox.style.display = "flex";
     }
 });
-var selectedEmoji = document.getElementsByClassName("selectEmoji");
-for (let i = 0; i < selectedEmoji.length; i++) {
-    selectedEmoji[i].addEventListener('click', (e) => {
-        input_bar.value += e.target.innerHTML;
-    })
-}
+var messageContainer = document.getElementById("chat-content")
 function changeUserName(e) {
     console.log(e.target);
     var userName = document.querySelector("#user-name>h2");
     userName.innerHTML = e;
+    curUserName = e;
     if (window.innerWidth < 625) {
         var contentDisp1 = document.getElementById("grid-item-1");
         var contentDisp2 = document.getElementById("grid-item-2");
@@ -31,13 +41,16 @@ function changeUserName(e) {
             contentDisp2.style.display = "none";
         })
     }
+    firebase.database().ref(`student/testglacom`).on("value",function (snap){
+        messageContainer.innerHTML = '';
+        if(snap.val().chats && snap.val().chats[curUserName]){
+            user = snap.val().chats[curUserName];
+            for(let i = 0; i < user.length; i++){
+                    addMessage(user[i]);
+                }
+        }
+    })
 }
-window.onload = () => {
-    var userName = document.querySelector("#user-name>h2");
-    let fName = document.getElementById("name");
-    userName.innerHTML = fName.innerText;
-}
-
 function searchByName() {
     let chatBody = document.getElementsByClassName('chatHeadingBody');
     for (let i = 0; i < chatBody.length; i++) {
@@ -46,7 +59,7 @@ function searchByName() {
         let filter = inputedString.value;
         let chatMessage = document.getElementsByClassName("chat-message");
         let userName = chatBody[i].firstElementChild.firstElementChild.innerHTML;
-        if(userName.toString().match(new RegExp('.*' + filter + '.*', "i"))){
+        if (userName.toString().match(new RegExp('.*' + filter + '.*', "i"))) {
             chatMessage[i].style.display = "";
             chatMessage[i].attributes[0].value = "";
         }
@@ -55,25 +68,39 @@ function searchByName() {
         }
     }
     inputedString = document.getElementsByClassName('search');
-    
+
 }
 function sendMessage(e) {
     e.preventDefault();
-    let hr = (new Date().getHours()%12);
+    let hr = (new Date().getHours() % 12);
     let min = new Date().getMinutes();
-    time = ((hr<10)?"0":"")+hr+ ":"+((min<10)?"0":"")+min+ ((new Date().getHours()< 12)?" am":" pm");
-    if(input_bar.value === ""){
+    if (input_bar.value === "") {
         return;
     }
-    let messageContainer = document.getElementById("chat-content");
+    time = ((hr < 10) ? "0" : "") + hr + ":" + ((min < 10) ? "0" : "") + min + ((new Date().getHours() < 12) ? " am" : " pm");
+    addMessage({message: input_bar.value, time: time});
+    let index = -1;
+    firebase.database().ref(`student/testglacom/`).on("value", function (snap){
+        if(snap.val().chats && snap.val().chats[curUserName]){
+            index = snap.val().chats[curUserName].length;
+        }
+    });
+    if(index === -1) index = 0;
+    firebase.database().ref(`student/testglacom/chats/${curUserName}/${index}`).set(
+        {
+            message:input_bar.value,
+            time: time
+        }
+    )
+    input_bar.value = "";
+}
+function addMessage(data){
     let newElement = document.createElement("div");
-    newElement.classList.add("chatBox");
     let childMessage = document.createElement("div");
+    newElement.classList.add("chatBox");
     childMessage.classList.add("messageDiv");
-    childMessage.innerHTML = `${input_bar.value}<br> <p id = "messageTime">${time}</p>`;
+    childMessage.innerHTML = `${data.message}<br> <p id = "messageTime">${data.time}</p>`;
     newElement.appendChild(childMessage);
     messageContainer.appendChild(newElement);
-    input_bar.value = "";
-    
-    messageContainer.scrollTop=messageContainer.scrollHeight;
+    messageContainer.scrollTop = messageContainer.scrollHeight;
 }
